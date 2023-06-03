676379437f7b41092cf3eee172d6d25ea5da7037
Matt-Wang <53413699+Matt-wzy@users.noreply.github.com>
2023-06-03 09:38:58 +0800

    Update test_isconfigclass.py

--- tests/unittest/test_isconfigclass.py
+++ tests/unittest/test_isconfigclass.py
@@ -5,8 +5,8 @@ class TestIsConfigClass:
     def test_none(self):
         assert is_config_class(None) is False
 
-def test_class(self):
-    class A:
-        pass
+    def test_class(self):
+        class A:
+            pass
 
-    assert is_config_class(A) is False
+        assert is_config_class(A) is False

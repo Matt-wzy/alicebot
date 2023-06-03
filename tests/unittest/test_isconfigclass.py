3893f4329ad4a2612bb078f828397a37e7aa0d9e
Matt-Wang <53413699+Matt-wzy@users.noreply.github.com>
2023-06-03 09:26:16 +0800

    Update test_isconfigclass.py

--- tests/unittest/test_isconfigclass.py
+++ tests/unittest/test_isconfigclass.py
@@ -2,11 +2,11 @@ from alicebot.utils import is_config_class
 
 
 class TestIsConfigClass:
-def test_none(self):
-assert is_config_class(None) is False
+    def test_none(self):
+        assert is_config_class(None) is False
 
 def test_class(self):
-class A:
-pass
+    class A:
+        pass
 
-assert is_config_class(A) is False
+    assert is_config_class(A) is False

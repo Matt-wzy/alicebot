e561e7b7dcaabd81912e535d430e903bde3cbd42
Matt-Wang <53413699+Matt-wzy@users.noreply.github.com>
2023-06-03 09:25:38 +0800

    Create test_isconfigclass.py

---
+++ tests/unittest/test_isconfigclass.py
@@ -0,0 +1,12 @@
+from alicebot.utils import is_config_class
+
+
+class TestIsConfigClass:
+def test_none(self):
+assert is_config_class(None) is False
+
+def test_class(self):
+class A:
+pass
+
+assert is_config_class(A) is False
